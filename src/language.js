f21d5285949bb6ab5ab5323aa46d88611c41246b
Şenol Doğan <45736551+senoldogann@users.noreply.github.com>
2021-06-01 16:10:54 +0300

    Add files via upload

---
+++ src/language.js
@@ -0,0 +1,38 @@
+const tr = {
+    settings: 'Tweet Ayarları',
+    name:'Ad Soyad',
+    username: 'Kullanıcı adı',
+    quoteTweet:"Alıntı Tweet",
+    like: 'Beğeni',
+    verified: "Doğrulanmış Hesap",
+    yes:"Evet",
+    no:"Hayır",
+    getInformation:"Bilgileri Çek",
+    buttonPlaceholder:"Twitter Kullanıcı adını yazın",
+    createButton:"Oluştur",
+    downloadTweet:"Tweeti indir",
+    exampleTweetDescripton:"Bu alana örnek tweet gelecek"
+}
+
+const en = {
+    settings: 'Tweet Settings',
+    name:'Name Surname',
+    username: 'Username',
+    quoteTweet:"Quote Tweet",
+    like: 'Like',
+    verified: "Verified Account",
+    yes:"Yes",
+    no:"No",
+    getInformation:"Get Information",
+    buttonPlaceholder:"Type Twitter Username",
+    createButton:"Create",
+    downloadTweet:"Download Tweet",
+    exampleTweetDescripton:"Example tweets will come in this field"
+}
+
+const language ={
+    tr,
+    en
+}
+
+export {language};
